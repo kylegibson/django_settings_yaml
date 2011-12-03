7dd0c52f4c00b51752ebe0a2908bee552f6b9f5b
Kyle Gibson <kyle.gibson@frozenonline.com>
2011-12-02 19:07:47 -0500

    removed automatic uppercasing

--- django_settings_yaml/__init__.py
+++ django_settings_yaml/__init__.py
@@ -10,7 +10,7 @@ import string
 import os
 
 DEFAULT_ENV_PREFIX = "DJANGO_SETTINGS_ENV_"
-SECRET_KEY_FILE = "secret_key_file"
+SECRET_KEY_FILE = "SECRET_KEY_FILE"
 
 def load_yaml_settings(context, files):
     settings = {}
@@ -24,14 +24,14 @@ def load_yaml_settings(context, files):
 def read_write_secret_key_file(settings):
     try:
         with open(settings[SECRET_KEY_FILE]) as rfd:
-            settings["secret_key"] = rfd.read().strip()
-        return settings["secret_key"]
+            settings["SECRET_KEY"] = rfd.read().strip()
+        return settings["SECRET_KEY"]
     except IOError:
         try:
             from random import choice
-            settings["secret_key"] = ''.join([choice(string.letters + string.digits + string.punctuation) for i in range(50)])
+            settings["SECRET_KEY"] = ''.join([choice(string.letters + string.digits + string.punctuation) for i in range(50)])
             with open(settings[SECRET_KEY_FILE], "w") as wfd:
-                wfd.write(settings["secret_key"])
+                wfd.write(settings["SECRET_KEY"])
         except IOError:
             pass
 
@@ -49,14 +49,10 @@ def load(context, files, load_env = True, add_python_path = True):
     for key, val in os.environ.items():
         context["ENV_%s" % key] = val
     settings = load_yaml_settings(context, files)
-    if add_python_path and "pythonpath" in settings:
-        sys.path.extend([pp for pp in settings["pythonpath"]])
+    if add_python_path and "PYTHONPATH" in settings:
+        sys.path.extend([pp for pp in settings["PYTHONPATH"]])
     if load_env:
         settings.update(get_settings_from_env())
-    if "secret_key" not in settings and SECRET_KEY_FILE in settings:
+    if "SECRET_KEY" not in settings and SECRET_KEY_FILE in settings:
         read_write_secret_key_file(settings)
-    def upper_keys(d):
-        if type(d) is dict:
-            return dict((k.upper(), upper_keys(v)) for k, v in d.iteritems())
-        return d
-    return upper_keys(settings)
+    return settings
